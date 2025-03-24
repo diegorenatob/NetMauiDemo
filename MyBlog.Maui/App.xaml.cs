d0d49a4e9780121336548a3d51746297e2ceb043
diegorenatob <43478203+diegorenatob@users.noreply.github.com>
2025-03-23 23:48:16 -0300

    second commit

---
+++ MyBlog.Maui/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MyBlog.Maui;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
+
---
+++ MyBlog.Maui/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MyBlog.Maui;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
+
